b88ed26f1aa6db91ead6d39197bbdca7fb6877d6
JunaidHafeez21 <160257992+JunaidHafeez21@users.noreply.github.com>
2024-02-18 18:56:26 +0500

    Add files via upload

---
+++ Q19.js
@@ -0,0 +1,4 @@
+"use strict";
+// Q19. Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+let dinnerGuest = ["Kamran", "Saif", "Ahmer", "Arbaz"];
+console.log(`You are inviting ${dinnerGuest.length} people to dinner.`);
---
+++ Q19.ts
@@ -0,0 +1,5 @@
+// Q19. Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+let dinnerGuest: string[] = ["Kamran", "Saif", "Ahmer", "Arbaz"];
+
+console.log(`You are inviting ${dinnerGuest.length} people to dinner.`);
